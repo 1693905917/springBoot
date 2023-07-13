41dc21cfdf6fbe86bdc3a3b60954ef3201c32c9b
1693905917 <1693905917@qq.com>
2023-07-13 11:47:34 +0800

    springBoot7.13

--- springboot_08_ssmp/src/main/java/com/itheima/Springboot08SsmpApplication.java
+++ springboot_08_ssmp/src/main/java/com/itheima/Springboot08SsmpApplication.java
@@ -3,10 +3,14 @@ package com.itheima;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 @SpringBootApplication
 public class Springboot08SsmpApplication {
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
         SpringApplication.run(Springboot08SsmpApplication.class, args);
     }
 
